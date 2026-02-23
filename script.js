e30142221ef54721e5eb4aa68f602357ef02a00b
shinnk <sygys10293@gmail.com>
2026-02-23 10:42:58 +0900

    temp

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const TRACK_WIDTH = 70.0;
 const TRACK_BUFFER = 25.0;   
 const TRACK_HEIGHT = 5.0;   
 const DT = 0.016;
-const BASE_SPEED_MEAN = 10.0;
-const SPEED_VARIANCE = 10.0;
+const BASE_SPEED_MEAN = 5.0;
+const SPEED_VARIANCE = 5.0;
 
 const CONDITION_INTERVAL = 1.5;
 const CONDITION_SMOOTHING = 0.03;
 
-const SENSITIVITY_A = 8.0;
-const SENSITIVITY_B = 3.0;
+const SENSITIVITY_A = 4.0;
+const SENSITIVITY_B = 1.5;
 
 // --- 게임 상태 ---
 let gameState = 'setup';
