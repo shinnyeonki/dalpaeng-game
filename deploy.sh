4c0c2f19046e0d943ee338bd4896ce78d81994c9
shinnk <sygys10293@gmail.com>
2026-02-23 13:04:16 +0900

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,17 @@ fi
 echo "Removing old file"
 sudo rm -rf /var/www/dalpaeng.mmv.kr/index.html
 sudo rm -rf /var/www/dalpaeng.mmv.kr/script.js
+sudo rm -rf /var/www/dalpaeng.mmv.kr/simulation.js
+sudo rm -rf /var/www/dalpaeng.mmv.kr/world.js
 sudo rm -rf /var/www/dalpaeng.mmv.kr/style.css
+
 
 echo "Copying new dist to remote location..."
 sudo cp -r index.html /var/www/dalpaeng.mmv.kr/
 sudo cp -r script.js /var/www/dalpaeng.mmv.kr/
+sudo cp -r ssimulation.js var/www/dalpaeng.mmv.kr/
+sudo cp -r world.js /var/www/dalpaeng.mmv.kr/
 sudo cp -r style.css /var/www/dalpaeng.mmv.kr/
+
 
 echo "[5/5] Deployment complete."
